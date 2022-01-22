b802930402a0dfe03decbbe0e4f3233a8e5df2ef
kudrin59 <76205670+kudrin59@users.noreply.github.com>
2022-01-22 06:43:36 +0500

    Update

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import discord
 from discord.ext import commands
+import numpy as np
 
 import bridge
 from bridge import *
 
-client = commands.Bot(command_prefix='!')
+prefix = '!'
+client = commands.Bot(command_prefix=prefix)
+client.remove_command('help')
+
+users = []
+
+
+def load_users():
+    global users
+    try:
+        users = np.load('users.npy')
+    except FileNotFoundError:
+        np.save("users", users)
+    print("Загрузка пользователей!")
+
+
+def save_users():
+    global users
+    np.save("users", users)
+    print("Сохранение пользователей!")
+
+
+def get_user_ally(author_id):
+    global users
+    for user in users:
+        if int(user[0]) == author_id:
+            return user[1]
 
 
 @client.event
@@ -13,36 +40,87 @@ async def on_ready():
 
 
 @client.command()
-async def h(ctx, arg=None):
+async def help(ctx):
+    global prefix
+
+    emb = discord.Embed()
+
+    emb.add_field(name='{}reg <<ALLY>>'.format(prefix), value='Сохранить ALLYCODE')
+    emb.add_field(name='{}p <<ALLY>>'.format(prefix), value='Информация об игроке')
+    emb.add_field(name='{}ga <<ALLY>> <<ALLY>>'.format(prefix), value='Сравнить игроков')
+    await ctx.send(embed=emb)
+
+
+@client.command()
+async def reg(ctx, ally=None):
+    global users
+    info = "Сохранить ALLYCODE: !reg <<ALLY>>"
     author = ctx.message.author
+    author_id = ctx.message.author.id
+
+    if not ally:
+        await ctx.send(f'{author.mention} {info}\n')
+        return False
+
+    rez = ""
+    create = True
+
+    for user in users:
+        if user[0] == str(author_id):
+            user[1] = ally
+            rez = "Код перезаписан!"
+            create = False
+            break
+
+    if create:
+        user = [author_id, ally]
+        users.append(user)
+        rez = "Код сохранён!"
+
+    save_users()
     await ctx.send(f'{author.mention}\n'
-                   'Информация об игроке: !p <<ALLY>>\n'
-                   'Сравнить игроков: !ga <<ALLY>> <<ALLY>>')
+                   f'{rez}\n')
 
 
 @client.command()
-async def p(ctx, ally):
+async def p(ctx, ally=None):
+    info = "Информация об игроке: !p <<ALLY>>"
     author = ctx.message.author
-    if len(ally) > 0 and ally.isdigit():
-        date = bridge.player_info(ally)
-        await ctx.send(f'{author.mention}\n'
-                       f'{date}')
-    else:
-        await ctx.send(f'{author.mention}\n'
-                       'Информация об игроке: !p <<ALLY>>\n')
+    author_id = ctx.message.author.id
+
+    if not ally:
+        ally = get_user_ally(author_id)
+    if not ally:
+        await ctx.send(f'{author.mention} {info}\n')
+        return False
+
+    date = bridge.player_info(ally)
+    await ctx.send(f'{author.mention}\n'
+                   f'{date}')
 
 
 @client.command()
-async def ga(ctx, ally, ally2):
+async def ga(ctx, ally=None, ally2=None):
+    info = "Сравнить игроков: !ga <<ALLY>> <<ALLY>>"
     author = ctx.message.author
-    if len(ally) > 0 and ally.isdigit() and len(ally2) > 0 and ally2.isdigit():
-        allys = [ally, ally2]
-        date = bridge.players_vs(allys)
-        await ctx.send(f'{author.mention}\n'
-                       f'{date}')
-    else:
-        await ctx.send(f'{author.mention}\n'
-                       'Сравнить игроков: !ga <<ALLY>> <<ALLY>>\n')
+    author_id = ctx.message.author.id
 
+    if not ally or not ally2:
+        if not ally:
+            ally = get_user_ally(author_id)
+        else:
+            ally2 = get_user_ally(author_id)
+
+    if not ally or not ally2:
+        await ctx.send(f'{author.mention} {info}\n')
+        return False
+
+    allys = [ally, ally2]
+
+    date = bridge.players_vs(allys)
+    await ctx.send(f'{author.mention}\n'
+                   f'{date}')
+
+load_users()
 TOKEN = open('token.txt', 'r').readline()
 client.run(TOKEN)
